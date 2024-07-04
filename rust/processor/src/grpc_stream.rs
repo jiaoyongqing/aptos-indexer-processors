93d12d915d2705a89ef8d707a11963a6651796cd
yongqing.jiao <657974959@qq.com>
2024-07-04 14:23:26 +0800

    fix

--- rust/processor/src/grpc_stream.rs
+++ rust/processor/src/grpc_stream.rs
@@ -651,8 +651,7 @@ pub async fn create_fetcher_loop(
                     stream_address = indexer_grpc_data_service_address.to_string(),
                     "[Parser] Reconnected more than {RECONNECTION_MAX_RETRIES} times. Will not retry.",
                 );
-                // panic!("[Parser] Reconnected more than {RECONNECTION_MAX_RETRIES} times. Will not retry.")
-                reconnection_retries = 0;
+                panic!("[Parser] Reconnected more than {RECONNECTION_MAX_RETRIES} times. Will not retry.")
             }
             reconnection_retries += 1;
             info!(
